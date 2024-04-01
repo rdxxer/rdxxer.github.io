bf9a3d91d5d5ee711fbd08097d3f2f5586efcead
rdxxer <rdxxer@pm.me>
2024-04-01 19:11:27 +0900

    Add solar rotation calculator

---
+++ calc/solar-rotation/main.js
@@ -0,0 +1,19 @@
+const A = 14.713
+const B = -2.396
+const C = -1.787
+function getOmega(alt) {
+    let phi = alt * (Math.PI / 180)
+    let omega = A + B * (Math.sin(phi)) ** 2 + C * (Math.sin(phi)) ** 4
+    return 360/omega
+}
+let output = document.getElementById('answer')
+document.getElementById('run').addEventListener('click', (event) => {
+    console.log('a')
+    altitude = document.getElementById('altitude').value
+    if (!isNaN(altitude)) {
+        output.innerText = getOmega(Number(altitude))
+    }
+    else {
+        output.innerText = ''
+    }
+})
