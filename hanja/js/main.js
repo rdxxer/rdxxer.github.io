d6df33ad5d35db082c27a86c4ed12a94344606cf
RdxxeR <rdxxer@pm.me>
2024-05-13 22:06:34 +0900

    Add regex to link parser

--- hanja/js/main.js
+++ hanja/js/main.js
@@ -1,6 +1,6 @@
 Array.from(document.getElementsByClassName('hanja')).forEach(b => {
     b.addEventListener('click', e => {
-        let url = 'https://en.wiktionary.org/wiki/' + b.innerHTML.split()[0]
+        let url = 'https://en.wiktionary.org/wiki/' + b.innerHTML.split(/[( ]/)[0]
         window.open(url, '_blank');
     })
 })
